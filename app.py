1ff94a7589a78136b8a884dfd220759bfd5cdec8
Samarth Chaplot <samarthchaplot7@gmail.com>
2025-03-23 23:11:06 +0530

    Added Requirements.txt

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import requests
 from PIL import Image
 from io import BytesIO
 
-from sympy import im
-
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0 Safari/537.36"
 }
